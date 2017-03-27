088bbb4cf7383a505509b8cd5e00a5afcab74857
FrankLee94 <1368514123@qq.com>
2017-03-27 10:07:53 +0800

    6 kinds of service are built

--- build.py
+++ build.py
@@ -48,7 +48,7 @@ def get_profile(pkl_path):
 	return profile
 
 # activity dict initialization for all users
-# user's activity: default value is 'down'
+# user's activity: default value is 'off'
 # format: {id_1:{'0507': [24/PERIOD], '0508': []}, id_2}
 def activity_dict_init(profile):
 	activity_dict = {}
@@ -58,29 +58,29 @@ def activity_dict_init(profile):
 			key = '050' + str(i)
 		else:
 			key = '05' + str(i)
-		activity_date[key] = ['down' for j in range(24 / PERIOD)]
+		activity_date[key] = ['off' for j in range(24 / PERIOD)]
 	for i in range(4, 11):
 		if i < 10:
 			key = '060' + str(i)
 		else:
 			key = '06' + str(i)
-		activity_date[key] = ['down' for j in range(24 / PERIOD)]
+		activity_date[key] = ['off' for j in range(24 / PERIOD)]
 	for i in range(2, 9):
 		key = '070' + str(i)
-		activity_date[key] = ['down' for j in range(24 / PERIOD)]
+		activity_date[key] = ['off' for j in range(24 / PERIOD)]
 	for i in range(6, 13):
 		if i < 10:
 			key = '080' + str(i)
 		else:
 			key = '08' + str(i)
-		activity_date[key] = ['down' for j in range(24 / PERIOD)]
+		activity_date[key] = ['off' for j in range(24 / PERIOD)]
 
 	for key in profile:
 		activity_dict[key] = copy.deepcopy(activity_date)   # 不能使用赋值，否则都是使用同一个内存块
 	return activity_dict
 
 # identify service's type from one line
-# service's type: W, G, S, V, idle, the computer is already 'on', so no 'down' here
+# service's type: W, G, S, V, idle, the computer is already 'on', so no 'off' here
 def service_identify(item_list, software_dict, website_dict):
 	time_pass = 0.0
 	service_type = 'idle'
@@ -138,59 +138,71 @@ def service_identify(item_list, software_dict, website_dict):
 	return service_type, time_pass
 
 # merge service in one period
-# service_insert: list, element is set()
+# service_insert: double list, the second list contains tuple: (service, current_time)
 def service_merge(hour, minute, time_pass, service_type, service_insert):
 	current_time = hour + minute / 60 + time_pass / 3600
 	if current_time >= 24:
-		current_time = 23.90
+		current_time = 23.99
 	index = int(current_time / PERIOD)
-	service_insert[index].add(service_type)
+	record = (service_type, current_time)
+
+	service_already_count = len(service_insert[index])
+	if service_already_count == 0:    # no service insert yet
+		service_insert[index].append((record))
+	else:
+		if service_type == service_insert[index][-1][0]:
+			pass                     # same service's type merge
+		else:
+			service_insert[index].append((record))
 	return service_insert
 
+# calculating service last most in one period
+# record hour format: list, element is tuple, (service_type, current_time)
+def cal_service_last_most(record_hour, period_index):
+	service_last_most = 'idle'
+	time_count = {'W':0.0, 'G':0.0, 'S':0.0, 'V':0.0, 'idle':0.0} # measurement in hour
+
+	end_time = float(period_index + PERIOD)
+	record_length = len(record_hour)
+	if record_length == 1:
+		service_last_most = record_hour[0][0]
+		return service_last_most
+	else:
+		for i in range(record_length - 1):
+			service_type = record_hour[i][0]
+			time_count[service_type] += abs(record_hour[i][1] - record_hour[i+1][1])
+		time_count[record_hour[-1][0]] += abs(record_hour[-1][1] - end_time)
+
+	time_count['idle'] = 0
+	time_min = 0
+	for key, value in time_count.items():
+		if value > time_min:
+			service_last_most = key
+			time_min = value
+	if time_min == 0:
+		service_last_most = 'idle'
+	return service_last_most
+ 
 # activity build from service_insert
 # category: 17
 def activity_build(activity_dict, service_insert, date, user_id):
 	cate = 'None'
-	cate_W = '0'
-	cate_G = '0'
-	cate_S = '0'
-	cate_V = '0'
-	for i in range(24 / PERIOD):
-		temp_set = service_insert[i]
-		if len(temp_set) != 0:
-			if 'W' in temp_set:
-				cate_W = '1'
-			else:
-				cate_W = '0'
-			if 'G' in temp_set:
-				cate_G = '1'
-			else:
-				cate_G = '0'
-			if 'S' in temp_set:
-				cate_S = '1'
-			else:
-				cate_S = '0'
-			if 'V' in temp_set:
-				cate_V = '1'
-			else:
-				cate_V = '0'
-			cate = cate_W + cate_G + cate_S + cate_V
-
-			if cate == '0000' and 'idle' in temp_set: # only in this case can we call this period 'idle'
-				cate = 'idle'
-
+	for i in range(24 / PERIOD):     # i: period index
+		record_hour = service_insert[i]
+		if len(record_hour) != 0:
+			cate = cal_service_last_most(record_hour, i)
 			activity_dict[user_id][date][i] = cate
-
-		else:       # leave out, default is down
+		else:       # leave out, default is off
 			pass
 	return activity_dict
 
 
 # build service for one user one day
+# this function is for one file
 def build_service(log_file_path, activity_dict, software_dict, website_dict, user_id):
 	log_file = open(log_file_path, 'r')
 	
-	service_insert = [set() for i in range(24 / PERIOD)]
+	service_insert = [[] for i in range(24 / PERIOD)]
 
 	line_num = 0
 	time_last = 0.0
@@ -208,7 +220,7 @@ def build_service(log_file_path, activity_dict, software_dict, website_dict, use
 			hour = float(item_list[3])
 			minute = float(item_list[4])
 			if hour + minute / 60 + time_last / 3600 > 24.0:
-				print 'time overflow'      # the log file cross two days
+				print 'time overflow'      # the log file cross two days, no time overflow occurs
 		else:
 			item_list = line.split('[=]')
 			service_type, time_pass = service_identify(item_list, software_dict, website_dict)
@@ -228,8 +240,8 @@ def pkl_save(activity_dict, save_path):
 
 
 if __name__ == '__main__':
-	software_path = './data/software_label_0322.txt'
-	website_path = './data/website_label_0322.txt'
+	software_path = '../data/software_label.txt'
+	website_path = '../data/website_label.txt'
 	software_dict, website_dict = build_dict(software_path, website_path)
 
 	root = 'C:/Users/lijialong94/Google Cloud/ML/project/data/behavior'
@@ -244,7 +256,7 @@ if __name__ == '__main__':
 			dirs_list.append(dirs)
 	dirs_list = dirs_list[0]
 
-	pkl_path = './data/profile.pkl'
+	pkl_path = '../data/profile.pkl'
 	profile = get_profile(pkl_path)
 	activity_dict = activity_dict_init(profile)
 
@@ -258,11 +270,11 @@ if __name__ == '__main__':
 			file_num += 1
 			print file_num
 
-	save_path = './data/activity_dict.pkl'
+	save_path = '../data/activity_dict.pkl'
 	pkl_save(activity_dict, save_path)
 
 	print 'length of dict: ' + str(len(activity_dict))
-	activity_temp = open('./data/activity_temp.txt', 'wb')
+	activity_temp = open('../data/activity_temp.txt', 'wb')
 	for key_0, value_0 in activity_dict.items():
 		activity_temp.write(key_0 + '\n')
 		for key, value in value_0.items():
