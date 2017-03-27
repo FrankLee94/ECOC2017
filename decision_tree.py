c9ec70b9608eaada48a64a1235de18dbedd3bb84
FrankLee94 <1368514123@qq.com>
2017-03-27 19:35:36 +0800

    apply oversampling method

--- decision_tree.py
+++ decision_tree.py
@@ -27,8 +27,7 @@ day_index = {'0507': 1, '0508': 2, '0509': 3, '0510': 4, '0511': 5, '0512': 6, '
 			 '0702': 1, '0703': 2, '0704': 3, '0705': 4, '0706': 5, '0707': 6, '0708': 0, 
 			 '0806': 1, '0807': 2, '0808': 3, '0809': 4, '0810': 5, '0811': 6, '0812': 0}
 
-service_type = ['idle', '0001', '0010', '0011', '0100', '0101', '0110', '0111', '1000',
-				'1001', '1010', '1011', '1100', '1101', '1110', '1111', 'down']
+service_type = ['idle', 'off', 'W', 'G', 'S', 'V']
 
 # get activity_dict
 def get_activity_dict(activity_dict_path):
@@ -78,31 +77,39 @@ def get_profile(profile_path):
 # select different features
 # feature format: [user_id, gender, age, edu, job, hour, date], 7 features
 # profile: dict, {user_id: [gender, age, edu, job]}
-def feature_select(data_dict, profile):
+def feature_select(data_dict, profile, over_sampling):
 	feature = []
 	cate = []
-
+	sampling_num = 80
 	user_count = 0  # convert user_id to integer
 	for user_id, all_dates in data_dict.items(): 
 		one_user_profile = copy.deepcopy(profile[user_id])  # gender, age, edu, job
 		one_user_profile.insert(0, user_count)        # insert user_id
 		for date, activity in all_dates.items():
 			for i in range(len(activity)):
-				if 1:#activity[i] != 'down':  # do not add 'down'
+				if 1:    #activity[i] != 'off':  # do not add 'off'
 					sample = copy.deepcopy(one_user_profile)
 					#del(sample[1:4])
-					sample.append(int(i/6))  #(int(i/6))                # i represents hour
+					sample.append(int(i/6))  #(int(i/6))        # i represents hour
 					sample.append(day_index[date])  # day_index: 7 days in one week
 					feature.append(sample)
 					cate.append(activity[i])
+					if over_sampling and len(sample) > 5:  # make sure that features are completed
+						if activity[i] != 'off':
+							sample_over = [[] for k in range(sampling_num)]
+							for j in range(sampling_num):
+								sample_over[j] = copy.deepcopy(sample)
+								sample_over[j][-3] = random.randint(0, 8)  # random disturbance in job feature
+								feature.append(sample_over[j])
+								cate.append(activity[i])
 		user_count += 1
 
 	return feature, cate
 
 # build features, all features
 def feature_build(train_dict, test_dict, profile):
-	feature_train, cate_train = feature_select(train_dict, profile)
-	feature_test, cate_test = feature_select(test_dict, profile)
+	feature_train, cate_train = feature_select(train_dict, profile, True)
+	feature_test, cate_test = feature_select(test_dict, profile, False)
 	return feature_train, feature_test, cate_train, cate_test
 
 # calculating the hit rate
@@ -114,6 +121,30 @@ def cal_hit_rate(cate_predict, cate_test):
 			hit_count += 1
 	hit_rate = float(hit_count) / float(sample_test_count)
 	return hit_rate
+
+# calculating F value
+def calculating_F_value(cate_predict, cate_test):
+	n_predict = 0
+	n_origin = 0
+	hit_count = 0
+	for item in cate_predict:
+		if item != 'off':
+			n_predict += 1
+	for item in cate_test:
+		if item != 'off':
+			n_origin += 1
+	for i in range(len(cate_predict)):
+		if cate_predict[i] != 'off' and cate_predict[i] == cate_test[i]:
+			hit_count += 1
+	precision = float(hit_count) / float(n_predict)
+	recall = float(hit_count) / float(n_origin)
+	F_value = 2 * precision * recall / (precision + recall)
+	print 'n_predict: ' + str(n_predict)
+	print 'n_origin: ' + str(n_origin)
+	print 'precision: ' + str(round(precision, 3))
+	print 'recall: ' + str(round(recall, 3))
+	print 'F_value: ' + str(round(F_value, 3))
+
 
 # Attention: When you use this method, make sure that only 2 features are selected!! [user_id, hour]
 # 1. select the service type using most in that period in past days
@@ -172,7 +203,7 @@ def conventional_method(feature_train, feature_test, cate_train):
 			value_1 = service_past[key_1]
 			cate_predict.append(value_1[0])
 		else:
-			random_num = random.randint(0, 15)
+			random_num = random.randint(0, len(service_type)-1)
 			cate_predict.append(service_type[random_num])
 
 	return cate_predict
@@ -211,17 +242,17 @@ def decision_tree(feature_train, feature_test, cate_train):
 # apply decision tree after cluster
 def decision_tree_cluster(n_feature_train, n_feature_test, n_cate_train, n_cate_test, n_clusters):
 	hit_rate_sum = 0.0
-	non_down_predict = 0
+	non_off_predict = 0
 	for i in range(n_clusters):
 		clf = tree.DecisionTreeClassifier()
 		clf = clf.fit(n_feature_train[i], n_cate_train[i])
 		n_cate_predict = clf.predict(n_feature_test[i])
 		for item in n_cate_predict:
-			if item != 'down':
-				non_down_predict += 1
+			if item != 'off':
+				non_off_predict += 1
 		hit_rate_sum += cal_hit_rate(n_cate_predict, n_cate_test[i])
 	print str(round(hit_rate_sum / n_clusters, 4) * 100) + '%'
-	print 'non_down_predict: ' + str(non_down_predict)
+	print 'non_off_predict: ' + str(non_off_predict)
 	print 'total number of sample: ' + str(len(cate_test))
 
 # apply random forests after cluster
@@ -307,13 +338,13 @@ def random_forests(feature_train, feature_test, cate_train):
 
 if __name__ == '__main__':
 	'''
-	activity_dict_path = './data/activity_dict.pkl'
+	activity_dict_path = '../data/activity_dict.pkl'
 	activity_dict = get_activity_dict(activity_dict_path)
-	train_dict_path = './data/train_dict.pkl'
-	test_dict_path = './data/test_dict.pkl'
+	train_dict_path = '../data/train_dict.pkl'
+	test_dict_path = '../data/test_dict.pkl'
 	data_segement(activity_dict, train_dict_path, test_dict_path)
 	'''
-
+	
 	train_dict_path = '../data/train_dict.pkl'
 	test_dict_path = '../data/test_dict.pkl'
 	train_dict, test_dict = get_data(train_dict_path, test_dict_path)
@@ -336,32 +367,29 @@ if __name__ == '__main__':
 	# conventional method
 	#cate_predict = conventional_method(feature_train, feature_test, cate_train)
 
-	'''
+	
 	hit_rate = cal_hit_rate(cate_predict, cate_test)
 	hit_rate = 100.0 * round(hit_rate, 4)
 	print "hit_rate:" + '\t' + str(hit_rate) + '%'
-	print feature_train[1000]
-	print feature_test[300]
+	print 'feature_train sample: ' + str(feature_train[1000])
+	print 'feature_test sample: ' + str(feature_test[1000])
+
+	calculating_F_value(cate_predict, cate_test)
+	
 	'''
-	
-	
 	# cluster method
-	for i in range(100):
+	for i in range(50):
 		n_clusters = i + 1
 		print "n_clusters: " + str(n_clusters)
 		feature_train, feature_test, cate_train, cate_test = feature_build(train_dict, test_dict, profile)
 		n_feature_train, n_feature_test, n_cate_train, n_cate_test = cluster(feature_train, feature_test, cate_train, cate_test, n_clusters)
 		decision_tree_cluster(n_feature_train, n_feature_test, n_cate_train, n_cate_test, n_clusters)
-		non_down_test = 0
-		if i == 0:
-			for item in cate_test:
-				if item != 'down':
-					non_down_test += 1
-		print non_down_test
-	
+
+	'''
 	#random_forests_cluster(n_feature_train, n_feature_test, n_cate_train, n_cate_test, n_clusters)
 	
 
 	
 	#sample_index_train, sample_index_test = calculating_index(feature_train, feature_test)
 	#decision_tree_single(feature_train, feature_test, cate_train, cate_test, sample_index_train, sample_index_test)
+	
